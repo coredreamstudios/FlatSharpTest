d547fd055416963457ea783b494addeaea3dfce6
Core Dream Studios <admin@coredreamstudios.com>
2025-01-23 05:00:37 -0500

    Cleaned up, simplest format layout. Changed .Serialize() to .Write() and it seems to run.

--- FlatSharpTest/Basics.cs
+++
@@ -1,9 +0,0 @@
-﻿public class Basics : IFlatSharp
-{
-    public void Run()
-    {
-        MessageBox.Show("I'm running! Try to catch me?");
-
-        
-    }
-}
---
+++ FlatSharpTest/MainApp.cs
@@ -0,0 +1,24 @@
+﻿using FlatSharp;
+
+using System.Windows.Forms;
+
+namespace FlatSharpTest
+{
+    public partial class MainApp : Form
+    {
+        public MainApp()
+        {
+            InitializeComponent();
+
+            SerializeData();
+        }
+
+        private void SerializeData() 
+        { 
+            Person person = new Person();
+            int maxBytesNeeded = Person.Serializer.GetMaxSize(person);
+            byte[] buffer = new byte[maxBytesNeeded];
+            int bytesWritten = Person.Serializer.Write(buffer, person);
+        }
+    }
+}
